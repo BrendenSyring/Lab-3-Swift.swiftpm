67a7fa283d02694ffa8e1631bf52dd4560082464
Brenden S <s0544317@student.rockvalleycollege.edu>
2023-09-10 15:09:52 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello world from Brenden!")
+                .font(.system(size:36, weight: .bold, design: .rounded))
+                .foregroundColor(.purple)
+                
+        }
+    }
+}
